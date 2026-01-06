13878e69ba3c59aa238f07e537c49fd0a2bde9e3
Suleimanhussein <suleymaanxuseen24-gmailcom>
2026-01-06 11:33:48 +0300

    first commit

---
+++ Dugsiiye js/exercise1/index.js
@@ -0,0 +1,18 @@
+let a = 7;
+let b = 3;
+console.log(a + b);
+// console.log(a * b);
+// console.log(a - b);
+// console.log(a / b);
+
+let c = 2;
+let d = 3;
+console.log(c - d);
+
+let e = 3;
+let f = 5;
+console.log(e * f);
+
+let g = 600;
+let h = 20;
+console.log(g / h);
