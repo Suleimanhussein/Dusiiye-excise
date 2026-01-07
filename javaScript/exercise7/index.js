b8ab2ea49bef0e78abd4b1c52ca42a4e58d0d4e0
Suleimanhussein <suleymaanxuseen24-gmailcom>
2026-01-08 00:36:43 +0300

    added exercise 7

---
+++ javaScript/exercise7/index.js
@@ -0,0 +1,10 @@
+let colors = ["red", "blue", "green", "yellow"];
+console.log(colors);
+
+colors.pop();
+console.log(colors);
+colors.push("brown");
+console.log(colors);
+colors.unshift("green");
+console.log(colors);
+console.log(colors.length)
