7003d052b1b80fee5cd1843855de0e6c4421e23d
Suleimanhussein <suleymaanxuseen24-gmailcom>
2026-01-09 18:58:38 +0300

    added exercise 10

---
+++ javaScript/exercise10/index.js
@@ -0,0 +1,21 @@
+let books = [
+  {
+    title: "javaScript",
+    author: "jamac",
+    year: 1985,
+  },
+  {
+    title: "html",
+    author: "ali",
+    year: 1977,
+  },
+  {
+    title: "css",
+    author: "suleyman",
+    year: 1999,
+  },
+];
+
+console.log(books);
+console.log(books[0].title);
+console.log(books[1].author);
