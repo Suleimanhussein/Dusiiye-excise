344ea05e699381ba68ee8651853f1deef4324000
Suleimanhussein <suleymaanxuseen24-gmailcom>
2026-01-12 03:05:29 +0300

    added exercise 15

---
+++ javaScript/exercise15/index.js
@@ -0,0 +1,22 @@
+let student = [
+  {
+    name: "suleiman",
+    live: "mogadishu",
+  },
+  {
+    name: "suleiman",
+    live: "mogadishu",
+  },
+  {
+    name: "suleiman",
+    live: "mogadishu",
+  },
+];
+
+for (let key of student) {
+  console.log(key);
+}
+
+for (let i in student) {
+  console.log(student[i]);
+}
