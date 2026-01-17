9a5466b73cc595983a61dafa9c940bd798f3b027
Suleimanhussein <suleymaanxuseen24-gmailcom>
2026-01-17 22:00:56 +0300

    added exercise 16

---
+++ javaScript/exercise16/index.js
@@ -0,0 +1,13 @@
+let result = 100;
+if (result > 50) {
+  console.log("passes");
+} else {
+  console.log("failed");
+}
+
+let studentResult = 49;
+if (studentResult > 50) {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
